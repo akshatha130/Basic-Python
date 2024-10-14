c3f204f29edeb83e19cac0e1707b435beabeaf32
Akshatha HS <hsakshu9@gmail.com>
2024-10-14 09:12:13 +0530

    Create max_num.py

---
+++ max_num.py
@@ -0,0 +1,9 @@
+def max_of_two(x,y):
+    if (x.isdigit() and y.isdigit()):
+            if x > y:
+                return x
+            return y
+    else:
+         print("not a proper input")
+#print(max_of_two(-2,0)) #erro with isdigit
+print(max_of_two('A',4)) #error A is not defined
