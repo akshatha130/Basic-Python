67a330aec9dfb9a8f04433b0912d778dc0a1611f
Akshatha HS <hsakshu9@gmail.com>
2024-10-14 09:10:48 +0530

    Create maxof3_num_fun.py

---
+++ maxof3_num_fun.py
@@ -0,0 +1,13 @@
+def max_of_three(x,y,z):
+    if x>y:
+        if x>z:
+            return x
+        if y>z:
+            return y
+        else:
+            return z
+    
+
+
+#print("not a proper input")
+print(max_of_three(3,5,9))
